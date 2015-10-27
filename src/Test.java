cb7be86d1d03a3742f7d81b68dadc5df2a20024d
Stefan Gustafsson <Stefangustaf@live.se>
2015-10-27 09:48:26 +0100

    test test

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,15 @@
 public class Test {
 	
 	
-     public void io(){
+	public static void main(String[] args) {
+		Test test = new Test();
+				test.io();
+	}
 	
+     public void io(){
+	for(int i =0; i < 100; i++){
+		int sum =i;
+		System.out.println(sum);
+	}
 }
 }
